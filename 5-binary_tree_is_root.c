849b448021e2c46bb0ed183772e59d624fcfdc73
ammarhass <ammar.hass94@gmail.com>
2024-05-07 12:07:17 +0300

    Task 5

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,11 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_root - function that checks if a given node is a root
+ * @node: Pointer to node being checked
+ * Return: 1 if node is root, 0 if NULL or not
+ */
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	return (node && !node->parent? 1 : 0);
+}
